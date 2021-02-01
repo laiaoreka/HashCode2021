a920282812d5e32080624d50fee57707f518c961
Laia G. Vernet <laiagvernet@MacBook-MacBook-Pro-de-Laia.local>
2021-02-01 21:01:41 +0100

    Codi_Exemple_A

--- Example_A.py
+++ Example_A.py
@@ -1,14 +1,36 @@
 # Libraries:
-import os
 
-# Get current directory:
-dir_path = os.path.dirname(os.path.realpath(__file__))
+# Read data from first file:
+file = '/Users/laiagvernet/Documents/HashCode_practice/HashCode2021/Data_Files/a_example.in'
+data_file = open(file, "r")
 
-# Read input data files names from directory:
-for root, directories, files in os.walk(dir_path, topdown=False):
-    for name in sorted(files):
-        if name.endswith(".in"):
-            print(os.path.join(root, name))
+# Get first line from file:
+first_line = data_file.readline()
+
+# Get info from first line:
+n_pizza = first_line[0]
+n_t2 = first_line[2]
+n_t3 = first_line[4]
+n_t4 = first_line[6]
+
+#Codiii
+# Pas 0 - ordenar les pizzes
+##### objecte/array de pizzes que tinguin els mateixos ingredients
+##### hash objectes
+##### classe pizza per saber si té un ingredient en concret
+##### crear objecte pizza
+##### paquets de pizzes i distribuir
+# Pas 1 - calcular el número d'entregues que podem fer (combinacions possibles d'equips)
+# Pas 2 - combinacions de pizza segons les entregues
+##### Tenir en compte que hi ha pizzes iguals
+
+
+# Close data file:
+data_file.close()
+
+
+
+
 
 
 
