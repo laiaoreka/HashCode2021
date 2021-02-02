74f73558e89d0910321304dec1be6dc884068840
Laia G. Vernet <laiagvernet@MacBook-MacBook-Pro-de-Laia.local>
2021-02-02 18:43:15 +0100

    Codi_Exemple_A_Laia

--- Example_A.py
+++ Example_A.py
@@ -13,7 +13,38 @@ n_t2 = first_line[2]
 n_t3 = first_line[4]
 n_t4 = first_line[6]
 
-#Codiii
+
+# Create the class pizza:
+class Pizza:
+    def __init__(self, index, total_ing, ing):
+        self.index = index
+        self.total_ing = total_ing
+        self.ing = ing
+
+
+# Get list of pizzas:
+lines = data_file.readlines()[0:]
+f = [i.strip('\n').split(' ') for i in lines]
+ind = 0
+pizzas = []
+for pizza in f:
+    P = Pizza(ind, pizza[0], pizza[1:len(pizza)])
+    pizzas.append(P)
+#print(pizzas[0].ing)
+
+# Get ingredients of unique pizzas:
+unique = set()
+for i in range(len(pizzas)):
+    unique.add(tuple(sorted(pizzas[i].ing)))
+print(unique)
+
+
+
+
+
+
+
+# Codiii
 # Pas 0 - ordenar les pizzes
 ##### objecte/array de pizzes que tinguin els mateixos ingredients
 ##### hash objectes
@@ -27,13 +58,3 @@ n_t4 = first_line[6]
 
 # Close data file:
 data_file.close()
-
-
-
-
-
-
-
-
-
-
