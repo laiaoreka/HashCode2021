d268c79869622a9842cc5e3af578a365f5680777
ASL <albert.sanahuja@sdezaina.com>
2021-02-04 22:38:10 +0100

    changes on Teams class

--- Example_A.py
+++ Example_A.py
@@ -1,5 +1,6 @@
 # Libraries:
 import os
+import math
 from math import factorial
 
 ##NOTE: Alternative code by ASL
@@ -12,24 +13,38 @@ obj = {}  # empty global variable for containing objects in order to get all pro
 ##CLASSES
 ## He fet aquesta classe que faig servir més endavant que potser no cal o hi ha una altra manera de fer (segur!!!)
 class Teams:
-    def __init__(self, line):
+    def set( self, line ):
         self.tot_pizza = line[0]
-        self.by2 = {2: line[1]}
-        self.by3 = {3: line[2]}
-        self.by4 = {4: line[3]}
+        self.tms = {
+            'by2': {
+                'team': line[1]
+                , 'pax': 2 * int(line[1])
+            }
+            , 'by3': {
+                'team': line[2]
+                , 'pax': 3 * int(line[2])
+            }
+            , 'by4': {
+                'team': line[3]
+                , 'pax': 4 * int(line[3])
+            }
+        }
 
-    def get_two_px(self):
-        return self.by2.key*self.by2.value
+        self.total_pax = sum( int(val['pax']) for key, val in self.tms.items())
+    
+    def __init__(self):
+        return
+    #####################
+    ##NOTE: NOT NECESSARY
+    #def get_two_px(self):
+        #return #self.teams['by2'].pax
 
-    def get_three_px(self):
-        return self.by3.key*self.by3.value
+    #def get_three_px(self):
+        #return #self.teams['by3'].pax #self.by3.key*self.by3.value
 
-    def get_four_px(self):
-        return self.by4.key*self.by4.value
-
-    def get_tot_people(self):
-        return self.by2.key*self.by2.value + self.by3.key*self.by3.value + self.by4.key*self.by4.value
-
+    #def get_four_px(self):
+        #return #self.teams['by4'].pax #self.by4.key*self.by4.value
+    #####################
 
 class Pizza:
     def __init__(self, index, total_ing, ing):
@@ -39,6 +54,15 @@ class Pizza:
 
     def get_ingredients(self):
         return self.ing
+
+##TODO and THINK, class pizzas as collection of pizza
+class Pizzas:    
+    def __init__(self):
+        return
+##TODO and THINK, class ingredients as pizza's subclass
+class Ingredients:    
+    def __init__(self):
+        return
 
 
 ##FUNCTIONS
@@ -64,7 +88,8 @@ def get_index_pos(list_of_elems, element):
 
 ##VARIABLES
 list_ing = {}  # List containing Objects from Pizza class
-
+T = Teams() # init teams class as global variable
+#P = Pizzas()
 
 # TODO: Getting exceptions??
 with open(fn) as f:
@@ -78,8 +103,8 @@ with open(fn) as f:
 
             ##NOTE: Getting Line information
             if cnt == 0:
-                ##NOTE: Get header information
-                T = Teams(line)
+                ##NOTE: Get header information and set to Teams
+                T.set(line)
             else:
                 ##NOTE: Create Pizza Objects
                 P = Pizza(cnt, line[0], line[1:len(line)])
@@ -91,13 +116,14 @@ with open(fn) as f:
             ##f.e.: obj[0] contains array with [5,1,2,1]
             # obj[cnt] = line.split(' ')
 
-    except:
+    except Exception as e:
         print('Some exception happened!')
-
+        print( e )
     finally:
         f.close()
 
-print(T.get_two_px())
+print('all teams: %s'%T.tms)
+print('get team info: teams?? ->%s pax?? ->%s'%(T.tms['by3']['team'],T.tms['by3']['pax']))
 
 ##NOTE: LGV --> aquests dos paràgrafs funcionen bé però s'han d'optimitzar que segur que hi ha una manera
 ## de fer-ho millor. La idea és cada fila de la llista uniq_list equival a una de les combinacions d'ingredients
