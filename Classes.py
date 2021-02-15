01c8ab704420512fa9c645baa7506a62ada04d7f
Laia G. Vernet <laiagvernet@MacBook-MacBook-Pro-de-Laia.local>
2021-02-15 21:16:48 +0100

    combinacions

--- Classes.py
+++ Classes.py
@@ -1,5 +1,5 @@
 from typing import Dict, Any, Union
-
+import Functions
 
 class Teams:
     def set(self, line):
@@ -44,4 +44,44 @@ class Ingredients(Pizzas):
         self.all_ings = something
 
     def __init__(self):
+        return
+
+
+class Deliveries:
+    def set(self, tot_pizza, tot_pax, by2, by3, by4):
+        if int(tot_pizza) > int(tot_pax):
+            self.delis = {'Total pizzas': str(tot_pax), 'del_by2': by2, 'del_by3': by3, 'del_by4': by4}
+        else:
+            self.delis = 0
+
+    def __init__(self):
+        return
+
+
+class Combis:
+    def set(self, pizzas):
+        self.pzs_ing = [d['Ingredients'] for d in pizzas]
+
+        self.comb = Functions.combinaciones(self.pzs_ing, 2)
+        for i in range(len(self.comb)):
+            common = len(np.intersect1d(self.comb[i][0], self.comb[i][1]))
+            unique = len(set(self.comb[i][0] + self.comb[i][1]))
+            self.combis_2.append({'Comb_id': i, 'Common ing': common, 'Uni ing': unique, 'Combi': self.comb[i]})
+
+        self.comb = Functions.combinaciones(self.pzs_ing, 3)
+        for i in range(len(self.comb)):
+            #common =
+            unique = len(set(self.comb[i][0] + self.comb[i][1] + self.comb[i][2]))
+            self.combis_3.append({'Comb_id': i, 'Common ing': common, 'Uni ing': unique, 'Combi': self.comb[i]})
+
+        self.comb = Functions.combinaciones(self.pzs_ing, 4)
+        for i in range(len(self.comb)):
+            #common =
+            unique = len(set(self.comb[i][0] + self.comb[i][1] + self.comb[i][2] + self.comb[i][3]))
+            self.combis_4.append({'Comb_id': i, 'Common ing': common, 'Uni ing': unique, 'Combi': self.comb[i]})
+
+    def __init__(self):
+        self.combis_2 = []
+        self.combis_3 = []
+        self.combis_4 = []
         return
--- Example_A.py
+++ Example_A.py
@@ -1,10 +1,12 @@
 # Libraries:
 import os
+import itertools
 import Classes
+import Functions
 
 ##NOTE: Relative path <-- Without problems to get files beetwen differents OS
 p = os.path.dirname(__file__)  # path as p
-fn = os.path.join(p, 'Data_Files/c_many_ingredients.in')  # File Name as fn
+fn = os.path.join(p, 'Data_Files/a_example.in')  # File Name as fn
 obj = {}  # empty global variable for containing objects in order to get all properties by line
 
 
@@ -12,7 +14,8 @@ obj = {}  # empty global variable for containing objects in order to get all pro
 list_ing = {}  # List containing Objects from Pizza class
 T = Classes.Teams()  # Init teams class as global variable
 P = Classes.Pizzas()
-I = Classes.Ingredients()
+D = Classes.Deliveries()
+C = Classes.Combis()
 
 
 # TODO: Getting exceptions??
@@ -45,6 +48,19 @@ with open(fn) as f:
         f.close()
 
 #print('All teams: %s' % T.tms)
-ingredients = set([item for sublist in P.ing for item in sublist])
-print(ingredients)
 # print('get team info: teams?? ->%s pax?? ->%s'%(T.tms['by3']['team'], T.tms['by3']['pax']))
+
+# Get list of unique ingredients:
+ingredients = set([item for sublist in P.ing for item in sublist])
+#print(ingredients)
+
+# Get number of deliveries:
+D.set(T.tot_pizza, T.total_pax, T.tms.get('by2').get('teams'), T.tms.get('by3').get('teams'), T.tms.get('by4').get('teams'))
+#print(D.delis)
+C.set(P.pizzas)
+# Aquest for és només per visualitzar:
+for i in range(len(C.combis_3)):
+    print(C.combis_3[i])
+
+
+
