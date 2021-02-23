7a1ca1d8a3e992ac3fa5b7d404ba575ed0deb196
Laia G. Vernet <laiagvernet@MacBook-MacBook-Pro-de-Laia.local>
2021-02-23 23:46:22 +0100

    deliveries + combis of 2

--- Example_A.py
+++ Example_A.py
@@ -1,41 +1,35 @@
 # Libraries:
 import os
-import Classes as c
-from itertools import permutations
+import Classes as Cl
+import itertools as it
 
-##NOTE: Relative path <-- Without problems to get files beetwen differents OS
+# Relative path <-- Without problems to get files beetwen differents OS
 p = os.path.dirname(__file__)  # path as p
-fn = os.path.join(p, 'Data_Files/d_many_pizzas.in')  # File Name as fn
+fn = os.path.join(p, 'Data_Files/a_example.in')  # File Name as fn
 obj = {}  # empty global variable for containing objects in order to get all properties by line
 
+# Class objects initializations
+T = Cl.Teams()
+P = Cl.Pizzas()
+D = Cl.Deliveries()
+U = Cl.Unique()
 
-##VARIABLES
-list_ing = {}  # List containing Objects from Pizza class
-T = c.Teams()  # Init teams class as global variable
-P = c.Pizzas()
-D = c.Deliveries()
-U = c.Unique()
-
-
-# TODO: Getting exceptions??
+# 0 - Get file information:
 with open(fn) as f:
     try:
         line = f.readline()
         cnt = 0
         while line:
-            ##NOTE: Show Line information
+            # Show Line information
             line = line.strip('\n').split(' ')
             # print('Line {}: {}'.format(cnt, line))
 
-            ##NOTE: Getting Line information
+            # Getting Line information
             if cnt == 0:
-                ##NOTE: Get header information and set to Teams
+                # Get header information and set to Teams
                 T.set(line)
             else:
                 P.set(cnt, line)
-                ##NOTE: Create Pizza Objects
-                #P = Pizza(cnt, line[0], line[1:len(line)])
-                #list_ing[cnt] = P.get_ingredients()
 
             line = f.readline()
             cnt += 1
@@ -46,29 +40,37 @@ with open(fn) as f:
     finally:
         f.close()
 
-# 0 - Get teams information:
-#print('All teams: %s' % T.tms)
+# print('All teams: %s' % T.tms)
 
 # 1 - Get number of deliveries:
-perm = permutations([2, 3, 4])
+perm = it.permutations([2, 3, 4])
 D.set(perm, T.tot_pizza, T.total_pax, T.tms.get('by2').get('teams'), T.tms.get('by3').get('teams'), T.tms.get('by4').get('teams'))
-
-for i in D.all_delis:
-    print(i)
 
 # 2 - Get list of unique pizzas based on ingredients:
 U.set(P.pizzas)
 unique = sorted(U.uni_pzs)
 unique.sort(key=len)
 
-j = len(unique)-1
-i = len(unique)-1
-k = 0
-combis = {}
-final2 = []
-print('Total unique pizzas: ', len(unique))
-#print('Total de 2: ', D.delis['del_by2'])
-tot_pzs_2 = str(1)#D.delis['del_by2']
+# initialize lists
+uni_comb = []
+uni_ing = []
+
+[indx, ings] = [[d['Pizza'] for d in P.pizzas], [d['Ingredients'] for d in P.pizzas]]
+permut = it.permutations(indx, 2)
+
+for comb in permut:
+    idx = sorted(list(comb))
+    if idx not in uni_comb:
+        uni_comb.append(idx)
+        ing_a = ings[int(idx[0])]
+        ing_b = ings[int(idx[1])]
+        unique = len(set(ing_a) ^ set(ing_b))
+        uni_ing.append({'Pizzas': idx, 'unique': unique})
+
+from operator import itemgetter
+newlist = sorted(uni_ing, key=itemgetter('unique'), reverse=True)
+print(newlist)
+
 
 
 
